d3979407a65d2837c4766e365f560f89ead0664c
MD HABIBUR RAHMAN <mdhabibur.hr7@gmail.com>
2024-09-03 19:13:31 +0600

    link , not-found page, loading page explored in next js

---
+++ src/app/loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center text-red-500 animate-pulse">
+        Loading...
+      </h1>
+    </div>
+  );
+};
+
+export default loading;
